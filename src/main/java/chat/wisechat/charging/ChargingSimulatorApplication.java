dac5ccd15a131728100085e0cc48735179b9fa8a
siberia.hu <wisechatteahouse@gmail.com>
2026-03-03 14:28:34 +0800

    init: 初始化项目

---
+++ src/main/java/chat/wisechat/charging/ChargingSimulatorApplication.java
@@ -0,0 +1,15 @@
+package chat.wisechat.charging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author siberia.hu
+ * @date 2026/3/3
+ */
+@SpringBootApplication
+public class ChargingSimulatorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChargingSimulatorApplication.class, args);
+    }
+}
---
+++ src/main/java/chat/wisechat/charging/controller/ChargingController.java
@@ -0,0 +1,13 @@
+package chat.wisechat.charging.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author siberia.hu
+ * @date 2026/3/3
+ */
+@RestController
+@RequestMapping("/charging")
+public class ChargingController {
+}
