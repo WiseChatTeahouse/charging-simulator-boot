99615fe2210309336238b75e9fef1ded8677bd40
siberia.hu <wisechatteahouse@gmail.com>
2026-03-20 14:16:40 +0800

    feat: 乐观锁插件和分页插件

--- src/main/java/chat/wisechat/charging/config/MybatisPlusConfig.java
+++ src/main/java/chat/wisechat/charging/config/MybatisPlusConfig.java
@@ -1,6 +1,11 @@
 package chat.wisechat.charging.config;
 
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -12,4 +17,17 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @MapperScan("chat.wisechat.**.mapper")
 public class MybatisPlusConfig {
+
+    /**
+     * 添加分页插件、配置乐观锁插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL)); // 如果配置多个插件, 切记分页最后添加
+        // 如果有多数据源可以不配具体类型, 否则都建议配上具体的 DbType
+        return interceptor;
+    }
+
 }
